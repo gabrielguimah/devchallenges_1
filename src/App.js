d4800f06ce914e1df7f25720012c5e9eed5b5fe5
Gabriel <gabrielguimah@gmail.com>
2022-07-26 23:51:08 -0300

    Commit initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import Scarecrow from './Scarecrow.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div className="container">
+        <header>
+          <h2>404 NOT FOUND</h2>
+        </header>
+        <div className="content"> 
+          <img src={Scarecrow} alt="Scarecrow"/>
+          <div className="information">
+            <h1>I have bad news for you</h1>
+            <p>The page you are looking for might be removed or is temporarily unavailable</p>
+            <button>Back to home</button>
+          </div>
+        </div>
+      </div>
+      <footer>
+        <p>created by gabrielguimah - devChallenges.io</p>
+      </footer>
+    </body>
   );
 }
 
